f70f290bf485380e2d2777733e70a46876c5f392
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-06-17 13:08:46 +0200

    2024-6-17
    application front end working and build

---
+++ README.md
@@ -0,0 +1,27 @@
+# arabidopsis-multi-app
+
+- a arabidopsis multipage desktop application electron, slint, javascript.
+- all functionalities of TAIR with in a single desktop application.
+- interacts with TAIR API.
+
+```
+npm install electron --save --legacy-peer-deps
+https://github.com/gauravcodepro/arabidopsis-multi-app.git
+cd arabidopsis-multi-app
+npm start
+```
+- to build the deb and rpm package 
+```
+git clone https://github.com/gauravcodepro/arabidopsis-multi-app.git
+sudo apt-get install rpm
+sudo npx electron-forge import
+npm run make
+```
+- the application compiled will be located in the out folder with the deb and the rpm builds and macos builds.
+  
+Gaurav \
+Academic Staff Member \
+Bioinformatics \
+Institute for Biochemistry and Biology \
+University of Potsdam \
+Potsdam,Germany
---
+++ src/main.js
@@ -0,0 +1,71 @@
+/*
+  electron app init for the arabidopsis package
+  Author Gaurav Sablok
+  Universitat Potsdam
+  Date: 2024-6-16
+ */
+
+const electron = require('electron');
+
+const url = require('url');
+
+const path = require('path')
+
+const {app, BrowserWindow, Menu} = electron;
+
+let main;
+/*
+const menuTemplate = [
+    {
+        label: "Arabidopsis information"
+    },
+    {
+        label: "Genotypes"
+    },
+    {
+        label: "Ontologies"
+    },
+    {
+        label:"Enrichment"
+    },
+    {
+        label: "Genomtrack visualization"
+    },
+    {
+        label:"Exit Arabidopsis"
+    },
+    {
+        label: "Analyze your data",
+        
+        submenu:[
+            {
+                label: "upload your gene ontology"
+            },
+            {
+                label: "upload your Arabidopsis AGI"
+            },
+            {
+                label: "convert your AGI to NCBI"
+            },
+            {
+                label: "analyze your AGI"
+            }   
+        ]
+    }
+]
+*/
+app.on('ready', () => {
+
+    // const menu = Menu.buildFromTemplate(mainMenu);
+
+   // Menu.setApplicationMenu(menu);
+
+    main = new BrowserWindow({});
+
+    main.loadURL(url.format({
+        pathname: path.join(__dirname, "main.html"),
+        protocol: 'file'
+
+    }));
+
+});
